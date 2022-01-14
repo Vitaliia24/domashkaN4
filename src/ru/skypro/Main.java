15780b718da5d660edc855c25b6a284bd90e2ae9
Vita <Vitayliyost@mail.ru>
2022-01-14 23:06:49 +0300

    исправила дз номер 4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,46 +1,44 @@
 package ru.skypro;
 
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
-	int i = 1;
-    while (i <= 10) {
-        System.out.print(i + " ");
-        i++;
+	taskOne();
+    taskTwo();
+    taskThree();
     }
+
+    public static void taskOne() {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
         System.out.println("\n");
-    for (int b= 10; b >= 1; b = b - 1) {
-        System.out.print(b + " ");
+        for (int b= 10; b >= 1; b = b - 1) {
+            System.out.print(b + " ");
+        }
     }
-    }
 
-
-
-
-    public static void main(String[] args) {
-
+    public static void taskTwo() {
         for (int friday = 5; friday <=31; friday = friday + 7) {
             System.out.println("Сегодня пятница - " + friday + "-е число. Необходимо подготовить отчет");
         }
     }
 
-
-
-    public static void main(String[] args) {
-
+    public static void taskThree() {
         int thisYear = 2021;
         int forTwoHandred = (thisYear - 200);
         int hundredYearsAfter = thisYear + 100;
 
         for (int i = 0; i < thisYear; i = i + 79) {
-    if (i < forTwoHandred) continue;
-    System.out.println(i);
-}
+            if (i < forTwoHandred) continue;
+            System.out.println(i);
+        }
         for (int i = 0; i < hundredYearsAfter; i = i + 79) {
             if (i < thisYear) continue;
             System.out.println(i);
         }
     }
+
 }
